3910e7ef0c36219afde81378e9026a9f3b94c55e
Glemhel <36306324+Glemhel@users.noreply.github.com>
2024-05-10 10:50:23 +0300

    noop

--- output/us_accidents.java
+++ output/us_accidents.java
@@ -2,7 +2,7 @@
 // WARNING: This class is AUTO-GENERATED. Modify at your own risk.
 //
 // Debug information:
-// Generated date: Tue May 07 00:36:54 MSK 2024
+// Generated date: Fri May 10 10:22:17 MSK 2024
 // For connector: org.apache.sqoop.manager.PostgresqlManager
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.Text;
